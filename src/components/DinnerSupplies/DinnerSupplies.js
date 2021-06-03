5c72531f77e64d5bf636ab501412cec5ff1449d7
TuckerLandis <landistuckerc@gmail.com>
2021-06-03 13:31:37 -0500

    silverware

--- src/components/DinnerSupplies/DinnerSupplies.js
+++ src/components/DinnerSupplies/DinnerSupplies.js
@@ -1,18 +1,24 @@
+import {useState} from 'react';
 
+import SilverWare from '../SilverWare/SilverWare';
 
 function DinnerSupplies ({guestList}) {
+
+function getCount(guestList) {
+  return guestList.length * 2;
+}
+
+let count = getCount(guestList)
+
     return (
         <>
             <h2>Dinner Supplies</h2>
-      <div>
-        Spoons: {guestList.length * 2}
-      </div>
-      <div>
-        Forks: {guestList.length * 2}
-      </div>
-      <div>
-        Knives: {guestList.length * 2}
-      </div>
+
+<SilverWare name="Spoons" count={count} />
+<SilverWare name="Forks" count={count} />
+<SilverWare name="Knives" count={count} />
+
+
         </>
     )
 }
---
+++ src/components/SilverWare/SilverWare.js
@@ -0,0 +1,11 @@
+function SilverWare (props) {
+
+
+    return (
+        <div>
+          <p> {props.name} : {props.count} </p>  
+        </div>
+    )
+}
+
+export default SilverWare
